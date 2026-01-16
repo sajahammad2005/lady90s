2d5f21bd40221da4a886899830c832dc5a68681e
sajah <sajah@DESKTOP-FF9JFND>
2026-01-16 17:21:45 +0200

    add

--- DatabaseProject/src/application/Main.java
+++ DatabaseProject/src/application/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+    /////////saajaaaaaaa
 
     public static void main(String[] args) {
         launch(args);
